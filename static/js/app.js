da9ac94b24765914233ee1a4f622298144634f2b
JonathanBrown003 <jdbrown00003@gmail.com>
2021-11-29 17:36:21 -0600

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,15 @@ const tableData = data;
 // Reference the HTML table using D3
 var tbody = d3.select("tbody");
 
+function buildTable(data) {
+    tbody.html("");
+  }
 
-
+  data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
